48bb6fca44361403931f1ed00f3b8844761b9431
tjr226 <tjr226@gmail.com>
2019-04-01 19:42:30 -0400

    refactored views.py and newticklerform at top of page to match the preferred django form syntax. can't figure out how to name forms across the views.py and home.html yet

--- myproject/todoapp/forms.py
+++ myproject/todoapp/forms.py
@@ -1,11 +1,12 @@
-# from django import forms
-# from .models import TicklerItem
+from django import forms
+from .models import TicklerItem
 
-# class NewTicklerItemForm(forms.ModelForm):
-#     task = forms.CharField(widget=forms.Textarea(), max_length=4000)
+class NewTicklerItemForm(forms.ModelForm):
+    # task = forms.CharField(widget=forms.Textarea(), max_length=4000)
 
-#     class Meta:
-#         model = TicklerItem
-#         fields = ['title', 'task']
+    class Meta:
+        model = TicklerItem
+        # next line picks out the tickler_text field from the TicklerItem model
+        fields = ['tickler_text']
 
     
--- myproject/todoapp/views.py
+++ myproject/todoapp/views.py
@@ -4,16 +4,18 @@ from django.shortcuts import render, redirect
 # copied from django-boards
 from django.http import HttpResponse
 from .models import TicklerItem
-# from .forms import NewTicklerItemForm
+from .forms import NewTicklerItemForm
 
 
 def home(request):
+    new_task_form = NewTicklerItemForm(request.POST)
+
 
     # code to process form entries
     if request.method == 'POST':
         # code to absorb top form that creates new tasks
-        if 'new_task' in request.POST:
-            incoming_task_text = request.POST['new_task']
+        if 'tickler_text' in request.POST:
+            incoming_task_text = request.POST['tickler_text']
             new_tickler_task = TicklerItem.objects.create(tickler_text = incoming_task_text)
             return redirect('home')
         if 'completed_task' in request.POST:
@@ -22,7 +24,9 @@ def home(request):
         if 'push' in request.POST:
             print("push task button press")
             #
+    else:
+        new_tickler_form = NewTicklerItemForm()
 
     items = TicklerItem.objects.exclude(completed_boolean=True)
 
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html', {'items': items, 'form': new_tickler_form})
