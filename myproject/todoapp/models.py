7fb623d76f36a3ccaa1eceb375a7e3d9223c7eec
tjr226 <tjr226@gmail.com>
2019-04-10 15:15:33 -0400

    added hidden boolean to TicklerItem model

--- myproject/todoapp/models.py
+++ myproject/todoapp/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class TicklerItem(models.Model):
     
-    # contains
-    # -tickler_text (text of the item)
-    # -created_by   (user that created it)
-    # -next_update_date (ideally will be the next date that the tickler item should bubble up to inbox)
-
+    # text of todo - this must be unique
     tickler_text = models.TextField(max_length=4000, unique=True)
-    # created_by = models.ForeignKey(User, related_name='tickler_items', on_delete='CASCADE')
+
+    # next date it will come up in the system
     next_update_date = models.DateTimeField(auto_now_add=True)
+
+    # completed or not
     completed_boolean = models.BooleanField(default=False)
+
+    # hidden or not
+    hidden_boolean = models.BooleanField(default=False)
 
     def __str__(self):
         return self.tickler_text
