da3c0fb810437d6a6095efa0b42c00f4f539103b
tjr226 <tjr226@gmail.com>
2019-04-10 18:01:48 -0400

    finished refactoring object selection to use IDs, also added HTML to require positive integers in Push Task field

--- myproject/todoapp/views.py
+++ myproject/todoapp/views.py
@@ -30,7 +30,6 @@ def home(request):
             items.update(hidden_boolean=False)
         #next section switches hidden_boolean to true
         elif 'hide' in request.POST:
-            print("hide task button press")
             form = HideTaskButton(request.POST)
             if form.is_valid():
                 x = request.POST['item_id']
@@ -40,26 +39,22 @@ def home(request):
             return redirect('home')
         #next section changes the next_update_date by adding the input from the field
         elif 'push' in request.POST:
-            print("push task button press")
-
             form = PushTaskButton(request.POST)
             if form.is_valid():
-                x = request.POST['item_tickler_text']
+                x = request.POST['item_id']
                 days_to_push = request.POST['days_to_push']
-                tickler_to_update = TicklerItem.objects.get(tickler_text=x)
+                tickler_to_update = TicklerItem.objects.get(id=x)
                 tickler_to_update.next_update_date = datetime.now() + timedelta(days=int(days_to_push))
                 tickler_to_update.save()
 
             return redirect('home')
-            #
         
         # this section flips the completed boolean
         elif 'complete' in request.POST:
-            print("complete task button press")
             form = CompleteTaskButton(request.POST)
             if form.is_valid():
-                x = request.POST['item_tickler_text']
-                tickler_to_update = TicklerItem.objects.get(tickler_text=x)
+                x = request.POST['item_id']
+                tickler_to_update = TicklerItem.objects.get(id=x)
                 tickler_to_update.completed_boolean = True
                 tickler_to_update.save()
 
